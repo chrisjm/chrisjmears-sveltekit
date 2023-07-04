4620d30d64367608c1ce2127e2733459f7d20bbd
Chris J Mears <chris.mears@gmail.com>
2023-07-04 05:00:27 +0000

    Fix MostRecentBlogPosts

--- src/lib/components/MostRecentBlogPosts.svelte
+++ src/lib/components/MostRecentBlogPosts.svelte
@@ -2,15 +2,14 @@
   import Section from "$lib/components/Section.svelte"
   import Header2 from "$lib/components/Heading2.svelte"
 
-  export let posts: any[]
-
-  $: console.log(posts);
+  // TODO: Replace `any`
+  export let posts: any[];
 </script>
 
 <Section sectionClasses="bg-blue-800 text-white">
   <Header2 title="Most Recent Posts" />
   <ul class="flex flex-col md:flex-row">
-    {#each posts as post (post.id)}
+    {#each posts as post (post.slug)}
       <li class="md:flex-1 md:mr-5">
         <a
           href="/blog/{post.slug}"
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import AboutMe from "$lib/components/AboutMe.svelte"
+  import NewsletterSignup from "$lib/components/NewsletterSignup.svelte"
+</script>
+
+<AboutMe />
+<NewsletterSignup />
