f652e8f50ef537aeeea0a4646fd9bf54d56438a5
Chris J Mears <chris.mears@gmail.com>
2023-07-04 21:10:08 +0000

    Wrap /blog in Section

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,52 +1,56 @@
 <script lang="ts">
   import type { PageData } from "./$types"
+  import Section from "$lib/components/Section.svelte"
   import { CldImage } from "svelte-cloudinary"
   export let data: PageData
   export let title: string
 </script>
 
-<div class="blog-list">
-  {#each data.allPosts as post (post.id)}
-    <div class="mb-10 leading-normal">
-      <a
-        class="underline hover:text-black text-blue-500 block"
-        href="/blog/{post.id}"
-      >
-        <h3 class="text-2xl md:text-3xl">
-          {title ?? post.data.metadata.title}
-        </h3>
-      </a>
-      <div class="mt-2 block text-sm text-gray-700">
-        {post.data.metadata.date}
-      </div>
-      {#if post.data.metadata.header.teaser}
-        <div class="mt-2 shadow-lg">
-          <a href="/blog/{post.id}">
-            {#if post.data.metadata.header.cloudinaryPublicId}
-              <CldImage
-                class="w-full h-48 object-cover"
-                src={post.data.metadata.header.cloudinaryPublicId}
-                width={1152}
-                height={192}
-              />
-            {:else}
-              <img
-                class="w-full h-48 object-cover"
-                src={post.data.metadata.header.teaser}
-                alt={`Featured`}
-              />
-            {/if}
-          </a>
+<Section>
+  <h1 class="text-4xl mb-6">Blog</h1>
+  <div class="blog-list">
+    {#each data.allPosts as post (post.id)}
+      <div class="mb-10 leading-normal">
+        <a
+          class="underline hover:text-black text-blue-500 block"
+          href="/blog/{post.id}"
+        >
+          <h3 class="text-2xl md:text-3xl">
+            {title ?? post.data.metadata.title}
+          </h3>
+        </a>
+        <div class="mt-2 block text-sm text-gray-700">
+          {post.data.metadata.date}
         </div>
-      {/if}
-      <div class="mt-2">
-        <p class="text-lg">
-          {post.data.metadata.description || post.data.metadata.excerpt}
-        </p>
-        <p class="text-body mt-1">
-          <a href="/blog/{post.id}">Read more.</a>
-        </p>
+        {#if post.data.metadata.header.teaser}
+          <div class="mt-2 shadow-lg">
+            <a href="/blog/{post.id}">
+              {#if post.data.metadata.header.cloudinaryPublicId}
+                <CldImage
+                  class="w-full h-48 object-cover"
+                  src={post.data.metadata.header.cloudinaryPublicId}
+                  width={1152}
+                  height={192}
+                />
+              {:else}
+                <img
+                  class="w-full h-48 object-cover"
+                  src={post.data.metadata.header.teaser}
+                  alt={`Featured`}
+                />
+              {/if}
+            </a>
+          </div>
+        {/if}
+        <div class="mt-2">
+          <p class="text-lg">
+            {post.data.metadata.description || post.data.metadata.excerpt}
+          </p>
+          <p class="text-body mt-1">
+            <a href="/blog/{post.id}">Read more.</a>
+          </p>
+        </div>
       </div>
-    </div>
-  {/each}
-</div>
+    {/each}
+  </div>
+</Section>
