b92e9e99dedc44b41b4e75a3b7f8ccc807b56aab
Chris Mears <chris.mears@gmail.com>
2025-08-18 22:15:16 -0700

    style: adjust blog post list item spacing and format category/tag links

--- src/lib/components/MostRecentBlogPosts.svelte
+++ src/lib/components/MostRecentBlogPosts.svelte
@@ -15,7 +15,7 @@
   <Header2 title="Most Recent Posts" />
   <ul class="flex flex-col md:flex-row">
     {#each posts as post (post.slug)}
-      <li class="md:flex-1 md:mr-5">
+      <li class="md:flex-1 md:mr-5 md:my-2 my-4">
         <a
           href="/blog/{post.slug}"
           rel="prev"
@@ -27,13 +27,12 @@
         </a>
         <div class="mt-2 flex flex-wrap gap-2">
           {#if post.frontmatter.categories}
-            {#each (Array.isArray(post.frontmatter.categories)
-              ? post.frontmatter.categories
-              : [post.frontmatter.categories]) as cat}
+            {#each Array.isArray(post.frontmatter.categories) ? post.frontmatter.categories : [post.frontmatter.categories] as cat}
               <a
                 href="/blog/category/{slugify(cat)}"
                 class="rounded-full border border-sky-300/70 px-2 py-0.5 text-xs text-sky-100 hover:bg-sky-600/30"
-              >{cat}</a>
+                >{cat}</a
+              >
             {/each}
           {/if}
           {#if Array.isArray(post.frontmatter.tags) && post.frontmatter.tags.length}
@@ -41,7 +40,8 @@
               <a
                 href="/blog/tag/{slugify(tag)}"
                 class="rounded-full bg-sky-600 px-2 py-0.5 text-xs text-white hover:bg-sky-500"
-              >#{tag}</a>
+                >#{tag}</a
+              >
             {/each}
           {/if}
         </div>
